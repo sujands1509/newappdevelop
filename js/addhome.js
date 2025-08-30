8aa6f4da69a9e6041f30f18464c1258f93fe8b3a
unknown <sujands1509@gmail.com>
2025-08-30 19:55:39 +0600

    payoo app complete

--- js/addhome.js
+++ js/addhome.js
@@ -1,25 +1,137 @@
+  
+  
+  // rsusable fucntion making important
 
+  function getInputValue(id){
+     const getInput = document.getElementById(id)
+     const getInputV = getInput.value 
+     const getInputVInt = parseInt(getInputV)
+
+    return getInputVInt;
+  }
+  
+  function getInputValueOnly(id){
+      const getInput = document.getElementById(id)
+      const getInputVOnly = getInput.value 
+     
+
+    return getInputVOnly;
+  }
+  
+// functions for innertext
+
+function getInnerText(id){
+  const getInnerT = document.getElementById(id)
+  const getInnerTvalue = getInnerT.innerText
+  const getInnerTextNumber = parseInt(getInnerTvalue)
+   return getInnerTextNumber;
+}
+
+  // funtions for set innerText  
+
+ function setInnerText(value){
+  const setInnerElement = document.getElementById("balance")
+  setInnerElement.innerText = value;
+
+  return setInnerElement;
+ }
+  
+
+//  fucntoin for toggle buttonm
+
+function toggleHandle(id){
+  const forms = document.getElementsByClassName("form")
+  for(const form of forms){
+    form.style.display= 'none';
+  }
+  document.getElementById(id).style.display="block";
+}
+
+
+
+  // add money js coding
 
 const validPin = 1234;
+const validAccount = 12345678910;
 
 document.getElementById("add-amount-btn").addEventListener('click',function(e){
-      e.preventDefault();
+  e.preventDefault()
+  const bank = getInputValueOnly("bank")
+  const accountNumber = getInputValueOnly("account-number")
 
-    const bank = document.getElementById("bank").value 
-    const accountNumber = document.getElementById("account-number").value 
-    const addAmount = parseInt(document.getElementById("add-amount").value )
-    const addPin = parseInt(document.getElementById("add-pin").value)
-    const availbleBalance = parseInt(document.getElementById("balance").innerText)
-    
-    if(accountNumber.length<11){
-      alert("please provide valid account number")
-      return;
-    }
+  // const amount = getInputValue("add-amount")
+  const amount = getInputValue("add-amount")
 
-    if(addPin !== validPin){
-      alert('please add valid pin code')
-      return;
-    }
-    const totalAvalableBalance = availbleBalance + addAmount;
-    document.getElementById("balance").innerText = totalAvalableBalance;
+  const pinNumber =  getInputValue("add-pin")
+  // const pinNumber = parseInt(document.getElementById("add-pin").value )
+
+  // const availableBalance = parseInt(document.getElementById("balance").innerText) 
+   const availableBalance = getInnerText("balance");
+
+  if(accountNumber.length<11){
+    alert("Invalid account Number");
+    return;
+  }
+
+  if(pinNumber!==validPin){
+    alert("Invalid pin Number");
+    return;
+  }
+
+  const totalAvailableBalance = amount + availableBalance;
+   document.getElementById("balance").innerText = totalAvailableBalance;
+})
+
+
+
+
+
+// add money js end
+
+// cashout js start
+
+document.getElementById("cashout-btn").addEventListener('click',function(e){
+  e.preventDefault()
+  // const agentNumber = document.getElementById("agent-number").value 
+  const agentNumber = getInputValueOnly("agent-number")
+  // const amount = parseInt(document.getElementById("cash-amount").value )
+
+  const amount = getInputValue("cash-amount")
+  // const pinNumber = parseInt(document.getElementById("cash-pin").value )
+
+  const pinNumber =getInputValue("cash-pin")
+  // const availableBalance = parseInt(document.getElementById("balance").innerText) 
+  const availableBalance = getInnerText("balance");
+
+
+  if(agentNumber.length<11){
+    alert("Invalid account Number");
+    return;
+  }
+
+  if(pinNumber!==validPin){
+    alert("Invalid pin Number");
+    return;
+  }
+
+  const totalAvailableBalance = availableBalance - amount ;
+   document.getElementById("balance").innerText = totalAvailableBalance;
+})
+
+
+// cashout js end
+
+// toggling js start
+document.getElementById("add-money-parent").addEventListener('click',function(){
+  toggleHandle("add-money-form");
+})
+document.getElementById("cashout-money-parent").addEventListener('click',function(){
+  toggleHandle("cashout-form");
+
+})
+
+document.getElementById("transfer-money-parent").addEventListener('click',function(){
+
+  toggleHandle("transfer-form");
+
 })
